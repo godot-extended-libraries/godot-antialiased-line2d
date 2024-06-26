ccfc40a5859d043f128b992043df82cd8095cb64
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-06-26 18:01:59 +0200

    Update for Godot 4.2.2

--- addons/antialiased_line2d/antialiased_line2d.gd
+++ addons/antialiased_line2d/antialiased_line2d.gd
@@ -1,14 +1,16 @@
-tool
-class_name AntialiasedLine2D, "antialiased_line2d.svg"
+@tool
+@icon("antialiased_line2d.svg")
+class_name AntialiasedLine2D
 extends Line2D
 
 
 func _ready() -> void:
 	texture = AntialiasedLine2DTexture.texture
 	texture_mode = Line2D.LINE_TEXTURE_TILE
+	texture_filter = TextureFilter.TEXTURE_FILTER_LINEAR_WITH_MIPMAPS_ANISOTROPIC
 
 
-static func construct_closed_line(p_polygon: PoolVector2Array) -> PoolVector2Array:
+static func construct_closed_line(p_polygon: PackedVector2Array) -> PackedVector2Array:
 	var end_point: Vector2 = p_polygon[p_polygon.size() - 1]
 	var distance: float = end_point.distance_to(p_polygon[0]) # distance to start point
 	var bridge_point: Vector2 = end_point.move_toward(p_polygon[0], distance * 0.5)
@@ -16,4 +18,5 @@ static func construct_closed_line(p_polygon: PoolVector2Array) -> PoolVector2Arr
 	var polygon_line := p_polygon
 	polygon_line.push_back(bridge_point)
 	polygon_line.insert(0, bridge_point)
+
 	return polygon_line
--- addons/antialiased_line2d/antialiased_regular_polygon2d.gd
+++ addons/antialiased_line2d/antialiased_regular_polygon2d.gd
@@ -1,18 +1,18 @@
 # This is a convenience node that automatically synchronizes an AntialiasedLine2D
 # with a Polygon2D, while also generating a regular Polygon2D shape (hexagon, octagon, …).
-tool
-class_name AntialiasedRegularPolygon2D, "antialiased_regular_polygon2d.svg"
+@tool
+@icon("antialiased_regular_polygon2d.svg")
+class_name AntialiasedRegularPolygon2D
 extends Polygon2D
 
-export var size := Vector2(64, 64) setget set_size
-export(int, 3, 128) var sides := 32 setget set_sides
-export(float, 0.0, 360.0) var angle_degrees := 360.0 setget set_angle_degrees
-
-export var stroke_color := Color(0.4, 0.5, 1.0) setget set_stroke_color
-export(float, 0.0, 1000.0) var stroke_width := 10.0 setget set_stroke_width
-export(int, "Sharp", "Bevel", "Round") var stroke_joint_mode := Line2D.LINE_JOINT_SHARP setget set_stroke_joint_mode
-export(float, 0.0, 1000.0) var stroke_sharp_limit := 2.0 setget set_stroke_sharp_limit
-export(int, 1, 32) var stroke_round_precision := 8 setget set_stroke_round_precision
+@export var size := Vector2(64, 64): set = set_size
+@export_range(3, 128) var sides: int = 32: set = set_sides
+@export_range(0.0, 360.0) var angle_degrees: float = 360: set = set_angle_degrees
+@export var stroke_color := Color(0.4, 0.5, 1.0): set = set_stroke_color
+@export_range(0.0, 1000.0) var stroke_width:float = 10.0: set = set_stroke_width
+@export var stroke_joint_mode:Line2D.LineJointMode = Line2D.LINE_JOINT_SHARP: set = set_stroke_joint_mode
+@export_range(0.0, 1000.0) var stroke_sharp_limit:float = 2.0: set = set_stroke_sharp_limit
+@export_range(1, 32) var stroke_round_precision: int = 8: set = set_stroke_round_precision
 
 var line_2d := Line2D.new()
 
@@ -20,20 +20,22 @@ var line_2d := Line2D.new()
 func _ready() -> void:
 	line_2d.texture = AntialiasedLine2DTexture.texture
 	line_2d.texture_mode = Line2D.LINE_TEXTURE_TILE
+	line_2d.texture_filter = TextureFilter.TEXTURE_FILTER_LINEAR_WITH_MIPMAPS_ANISOTROPIC
 	update_points()
 	add_child(line_2d)
 
 
-func _set(property: String, value) -> bool:
-	if property == "polygon":
+func _set(property: StringName, value: Variant) -> bool:
+	if property == &"polygon":
 		line_2d.points = AntialiasedLine2D.construct_closed_line(value)
+
 	return false
 
 
 func update_points() -> void:
-	var points := PoolVector2Array()
+	var points := PackedVector2Array()
 	for side in sides:
-		points.push_back(Vector2(0, -1).rotated(side / float(sides) * deg2rad(angle_degrees)) * size * 0.5)
+		points.push_back(Vector2(0, -1).rotated(side / float(sides) * deg_to_rad(angle_degrees)) * size * 0.5)
 	if not is_equal_approx(angle_degrees, 360.0):
 		points.push_back(Vector2.ZERO)
 	polygon = points
@@ -60,13 +62,12 @@ func set_stroke_color(p_stroke_color: Color) -> void:
 	stroke_color = p_stroke_color
 	line_2d.default_color = stroke_color
 
-
 func set_stroke_width(p_stroke_width: float) -> void:
 	stroke_width = p_stroke_width
 	line_2d.width = stroke_width
 
 
-func set_stroke_joint_mode(p_stroke_joint_mode: int) -> void:
+func set_stroke_joint_mode(p_stroke_joint_mode: Line2D.LineJointMode) -> void:
 	stroke_joint_mode = p_stroke_joint_mode
 	line_2d.joint_mode = stroke_joint_mode
 
